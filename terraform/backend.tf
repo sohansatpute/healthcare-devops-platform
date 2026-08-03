850a20e4ab3ccc6f980c2959e077d6d623e9d6f7
Sohan Satpute <satputesohan8@gmail.com>
2026-08-03 15:36:40 +0530

    Rebuild infrastructure on new AWS account

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -7,10 +7,10 @@ terraform {
 
   backend "s3" {
 
-    bucket         = "sohansatpute-terraform-state"
+    bucket         = "sohansatpute-healthcare-tfstate"
     key            = "healthcare-devops/terraform.tfstate"
     region         = "ap-south-1"
-    dynamodb_table = "terraform-state-lock"
+    use_lockfile = true
     encrypt        = true
 
   }
