42456018991f33ab5f775cc022b664e4f1fcf2e9
Sohan Satpute <satputesohan8@gmail.com>
2026-08-06 12:37:41 +0530

    Complete end-to-end CI/CD pipeline with Terraform, Jenkins, Docker and AWS

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -18,9 +18,9 @@ resource "aws_lb_target_group" "app_tg" {
     protocol            = "HTTP"
     matcher             = "200"
     interval            = 30
-    timeout             = 5
+    timeout             = 10
     healthy_threshold   = 2
-    unhealthy_threshold = 2
+    unhealthy_threshold = 5
 
   }
 
