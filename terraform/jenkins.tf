2f50ae3e8f56ffefa9cc580be37a0c2fde4de1c0
Sohan Satpute <satputesohan8@gmail.com>
2026-08-06 15:22:00 +0530

    Jenkins made secure HTTPS

--- terraform/jenkins.tf
+++ terraform/jenkins.tf
@@ -20,11 +20,20 @@ resource "aws_security_group" "jenkins_sg" {
 
   ingress {
 
-    description = "Jenkins"
+    description = "HTTPS for Jenkins"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
 
-    from_port = 8080
-    to_port   = 8080
-    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
 
     cidr_blocks = ["0.0.0.0/0"]
 
@@ -34,12 +43,11 @@ resource "aws_security_group" "jenkins_sg" {
 
     description = "Jenkins"
 
-    from_port = 8081
-    to_port   = 8081
+    from_port = 8080
+    to_port   = 8080
     protocol  = "tcp"
 
     cidr_blocks = ["0.0.0.0/0"]
-
 
   }
 
